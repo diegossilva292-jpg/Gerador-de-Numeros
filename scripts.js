d708e54d1988c1b6ca7bfa9e4a60b633e4682770
diegossilva292 <diegossilva292@gmail.com>
2026-03-27 16:12:10 -0300

    Meu Segundo commit

--- scripts.js
+++ scripts.js
@@ -4,8 +4,15 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const numero = Math.floor(Math.random() * (max - min) + min);
+if(min >= max){
+alert("O valor minimo tem que ser MENOR que o valor máximo")
+}
+
+else {
+   const numero = Math.floor(Math.random() * (max - min) + min);
 
   document.getElementById("resultado").innerText = numero;
+}
+
 
 }
